bfd77fa40e656139cca1d68eba6b9dc8360fb969
Vraj Satashia <vsata@seas.upenn.edu>
2023-12-01 19:33:56 -0500

    skeleton for traffic dynamics

---
+++ traffic_dynamics.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+class Traffic:
+    def __init__(self, sumo_file):
+        # initial state
+        self.N = 100 # number of links and controlling lights
+        self.x_0 = np.zeros(self.N)
+        self.xdot_0 = np.zeros(self.N)
+
+        # intial control (green time)
+        self.g_0 = np.zeros(self.N)
+
+        # traffic cycle conditions
+        self.C = 60 # length of a traffic cycle [s]
+        self.T = 1 # length of a discrete time step [s]
+        self.num_timesteps = 10000 # number of total timesteps in the simulation
+
+        # dynamics constants (extracted from SUMO)
+        self.S = np.zeros(self.N) # max saturation ie. max outflow when a light is green
+        self.B = 0 # constant matrix describing the relation between green time and the outlow
+        self.T = 1
+        self.D = 0
+
+    # function for parsing the SUMO file and creating the dynamics from it
+    def parse_sumo(self, sumo_file):
+        # TODO: learn the necessary dynamics from the SUMO file
+        pass
+
+    # Eqn. 5
+    def g_k(self, G_k):
+        S = 0 # TODO
+        C = self.C # TODO
+        return G_k @ S / C
+
+    # Eqn. 6
+    def x_kp1(self, x_k, g_k, d_k):
+        B = 0 # TODO
+        T = 0 # TODO
+        return x_k + B @ g_k + T * d_k
+
+    # getting the cost of a particular state
+    def calculate_cost(self, x_k, g_k):
+        Q = np.zeros((self.N, self.N)) # TODO
+        R = np.zeros((self.N, self.N)) # TODO
+        return x_k.T @ Q @ x_k + g_k.T @ R @ g_k
+
+
+
