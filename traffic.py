b397cabebfc5345edb5eb5cc45f3d8dae14d0910
Vraj Satashia <vsata@seas.upenn.edu>
2023-12-04 18:50:39 -0500

    merging into one class

--- traffic.py
+++ traffic.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pandas as pd
+from pydrake.systems.controllers import DiscreteTimeLinearQuadraticRegulator
+from pydrake.solvers import MathematicalProgram, Solve, OsqpSolver
+import pydrake.symbolic as sym
 
 class Traffic:
     def __init__(self, sumo_file):
@@ -22,17 +25,16 @@ class Traffic:
         self.num_timesteps = 10000 # number of total timesteps in the simulation
 
         # dynamics constants (extracted from SUMO)
-
         self.A = np.eye(self.N) # identity matrix on queue length
         self.S = np.zeros(self.N) # max saturation ie. max outflow when a light is green
-        self.B = np.zeros((self.N, self.N)) # constant matrix describing the relation between green time and the outlow
-        self.D = np.zeros((self.N, self.N)) # disturbance matrix
+        self.B = np.eye(self.N) # constant matrix describing the relation between green time and the outlow
+        self.D = np.eye(self.N) # disturbance matrix
         self.t_z0 = 0 # exit flow rate within a link
         self.d_z = 0 # disturbance flow rate within a link, could base it off the length of a link
 
         # costs
-        self.Q = np.ones((self.N, self.N)) # TODO: pick better costs to queue length
-        self.R = 10 * np.ones((self.N, self.N)) # TODO: pick better costs to green time, or switching
+        self.Q = np.eye(self.N) # TODO: pick better costs to queue length
+        self.R = np.eye(self.N) # TODO: pick better costs to green time, or switching
 
         # update the dynamics according to the SUMO file
         self.parse_sumo()
@@ -56,13 +58,7 @@ class Traffic:
             s_z = self.t_z0 * self.df['inflow'][z]
             d_z = self.d_z
             u_z = self.df['outflow'][z]
-            self.B[z][z] = T * (q_z - s_z + d_z - u_z) # Eqn. 3
-
-    # Eqn. 5
-    def g_k(self, G_k):
-        S = self.C
-        C = self.C 
-        return G_k @ S / C
+            self.B[z][z] = self.C * (q_z - s_z + d_z - u_z) # Eqn. 3
 
     # Eqn. 6
     def x_kp1(self, x_k, g_k):
@@ -76,11 +72,33 @@ class Traffic:
         Q = self.Q
         R = self.R
         return x_k.T @ Q @ x_k + g_k.T @ R @ g_k
+    
+    def compute_LQR_feedback(self, x_current):
+        A = self.A
+        B  = self.B
+        R = self.R
+        Q = self.Q
+
+        # Solve the discrete-time algebraic Riccati equation and get gains
+        K, optimal_cost = DiscreteTimeLinearQuadraticRegulator(A, B, Q, R)
+
+        # getting the greentime
+        g_current = -K @ x_current
+
+        # returns gains along with corresponding link ordering
+        return g_current, self.df['link'] # very finnicky, some values are returning negative, need to fix the cost function or the dynamics. Also adjust the scaling with a time cycle
+    
+    def MPC(self, traffic_object):
+        # TODO: use this to add constaints to the above LQR problem in OSQP
+        
+        pass  
 
 
 if __name__ == "__main__":
-    print("hello")
     traffic = Traffic("linkToFlows.csv")
     traffic.parse_sumo()
-    print(traffic.B)
+    # print(traffic.B)
+    x_current = np.ones((traffic.N, 1))
+    g_current, link_list = traffic.compute_LQR_feedback(x_current)
+    print(g_current)
 
