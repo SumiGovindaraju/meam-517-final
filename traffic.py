7122c6d277439981800e52a567d859cec4ce6ecc
Vraj Satashia <vsata@seas.upenn.edu>
2023-12-04 21:41:36 -0500

    added MPC, still working on constraints

--- traffic.py
+++ traffic.py
@@ -16,24 +16,20 @@ class Traffic:
         self.x_0 = np.zeros(self.N)
         self.xdot_0 = np.zeros(self.N)
 
-        # intial control (green time)
-        self.g_0 = np.zeros(self.N)
-
-        # traffic cycle conditions
+        # traffic cycle and timing conditions
         self.C = 60 # length of a traffic cycle [s]
-        self.T = 1 # length of a discrete time step [s]
-        self.num_timesteps = 10000 # number of total timesteps in the simulation
+        self.num_steps = 60 # number of steps in a cycle
+        self.T = self.C / self.num_steps # time step [s]
+        self.num_runs = 100 # number of cycles to simulate
 
         # dynamics constants (extracted from SUMO)
         self.A = np.eye(self.N) # identity matrix on queue length
-        self.S = np.zeros(self.N) # max saturation ie. max outflow when a light is green
         self.B = np.eye(self.N) # constant matrix describing the relation between green time and the outlow
-        self.D = np.eye(self.N) # disturbance matrix
-        self.t_z0 = 0 # exit flow rate within a link
-        self.d_z = 0 # disturbance flow rate within a link, could base it off the length of a link
+        self.d_k = np.zeros(self.N) # despawn rate
+        self.s_k = np.zeros(self.N) # spawn rate
 
-        # costs
-        self.Q = np.eye(self.N) # TODO: pick better costs to queue length
+        # cost matrices
+        self.Q = np.eye(self.N) # TODO: pick better costs to queue length (ie. 1 / x_z,max)
         self.R = np.eye(self.N) # TODO: pick better costs to green time, or switching
 
         # update the dynamics according to the SUMO file
@@ -51,28 +47,28 @@ class Traffic:
     # function for parsing the SUMO file and creating the dynamics from it
     def parse_sumo(self):
 
-        # defining the B matrix
+        # defining the B matrix based on historical data from the cycle horizon
         for z in range(self.N):
-            T = self.T
+            
+            B = np.zeros(self.N)
+
+            # coming into the links
             q_z = self.df['inflow'][z]
-            s_z = self.t_z0 * self.df['inflow'][z]
-            d_z = self.d_z
+
+            # leaving the link
             u_z = self.df['outflow'][z]
-            self.B[z][z] = self.C * (q_z - s_z + d_z - u_z) # Eqn. 3
 
-    # Eqn. 6
+            self.B[z][z] = (q_z - u_z) # Eqn. 3
+
+        # updating the spawn and despawn rates
+        self.d_k = np.array(self.df['despawns'])
+        self.s_k = np.array(self.df['spawns'])
+
+    # Eqn. 6, dynamics of the system
     def x_kp1(self, x_k, g_k):
-        B = self.B
-        T = self.B
-        d_k = self.d_z # assuming that the disturbance is time invariant
-        return x_k + B @ g_k + T * d_k
-
-    # getting the cost of a particular state
-    def cost(self, x_k, g_k):
-        Q = self.Q
-        R = self.R
-        return x_k.T @ Q @ x_k + g_k.T @ R @ g_k
+        return self.A @ x_k + self.B @ g_k + self.T * (self.s_k - self.d_k)
     
+    # standard LQR coming from linear dynamics and quadratic cost
     def compute_LQR_feedback(self, x_current):
         A = self.A
         B  = self.B
@@ -88,17 +84,70 @@ class Traffic:
         # returns gains along with corresponding link ordering
         return g_current, self.df['link'] # very finnicky, some values are returning negative, need to fix the cost function or the dynamics. Also adjust the scaling with a time cycle
     
-    def MPC(self, traffic_object):
-        # TODO: use this to add constaints to the above LQR problem in OSQP
-        
-        pass  
+    def add_initial_state_constraint(self, prog, x, x_current):
+        # initial state constraint.
+        prog.AddBoundingBoxConstraint(x_current, x_current, x[0])
+
+    def add_input_saturation_constraint(self, prog, x, g):
+        # limits on green time
+        for i in range(self.num_steps-1):
+            prog.AddBoundingBoxConstraint(0, self.C, g[i])
+
+
+    def add_dynamics_constraint(self, prog, x, g):
+        # adding dynamics constraints
+        for k in range(self.num_steps-1):
+            x_e = x[k]
+            g_e = g[k]
+            x_e_kp1 = x[k+1]
+            x_dyn_kp1 = self.x_kp1(x, g)
+
+            for i in range(self.N):
+                prog.AddLinearEqualityConstraint(x_dyn_kp1[i] == x_e_kp1[i])
+
+    def add_cost(self, prog, x, g):
+        # quadratic cost on the state and input
+        for k in range(self.num_steps - 1): 
+            x_e = x[k] 
+            g_e = g[k]
+            prog.AddQuadraticCost(x_e.T @ self.Q @ x_e + g_e.T @ self.R @ g_e)
+
+    def compute_MPC_feedback(self, x_current, use_clf=False):
+
+        # Parameters for the QP
+        N = self.N
+        T = self.T
+
+        # Initialize mathematical program and decalre decision variables
+        prog = MathematicalProgram()
+        x = np.zeros((N, 6), dtype="object")
+        for i in range(N):
+            x[i] = prog.NewContinuousVariables(6, "x_" + str(i))
+            g = np.zeros((N-1, 2), dtype="object")
+        for i in range(N-1):
+            g[i] = prog.NewContinuousVariables(2, "g_" + str(i))
+
+        # Add constraints and cost
+        self.add_initial_state_constraint(prog, x, x_current)
+        self.add_input_saturation_constraint(prog, x, g)
+        self.add_dynamics_constraint(prog, x, g)
+        self.add_cost(prog, x, g)
+
+        # Solve the QP
+        solver = OsqpSolver()
+        result = solver.Solve(prog)
+        g_mpc = result.GetSolution(u[0])
+
+        return g_mpc, self.df['link']
 
 
 if __name__ == "__main__":
     traffic = Traffic("linkToFlows.csv")
     traffic.parse_sumo()
-    # print(traffic.B)
-    x_current = np.ones((traffic.N, 1))
-    g_current, link_list = traffic.compute_LQR_feedback(x_current)
-    print(g_current)
+    print(traffic.B)
+    # x_current = np.ones((traffic.N, 1))
+    # g_lqr, link_list = traffic.compute_LQR_feedback(x_current)
+    # print("LQR RESULT: ", g_lqr)
+    g_mpc, link_list = traffic.compute_MPC_feedback(x_current)
+    print("MPC RESULT: ", g_mpc)
 
